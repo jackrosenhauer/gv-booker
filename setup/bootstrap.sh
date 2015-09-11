840fc9644a7b23ea42501cc5644db5f530e5061f
jackrosenhauer <jackrosenhauer@gmail.com>
2015-09-10 23:27:56 -0400

    eod commmit, adding mariadb

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -6,14 +6,20 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant/www /var/www
 fi
 
-apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+#mariadb
+apt-get install software-properties-common
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository 'deb http://mirrors.accretive-networks.net/mariadb/repo/10.0/ubuntu trusty main'
 
 apt-get -y update
+
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 
 apt-get install -y python2.7
 apt-get install -y python-pip
 apt-get install -y nodejs
+apt-get install -y mariadb-server
 
 #Python and pip packages
 pip install flask
